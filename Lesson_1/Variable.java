90549a37cd4eadd19af4dec76212efa54abf3449
Andrey Falkovskiy <avf-91@yandex.ru>
2021-11-29 23:10:15 +0300

    implement HW_1

---
+++ Lesson_1/Variable.java
@@ -0,0 +1,16 @@
+public class Variable {
+	public static void main(String[] args) {
+		byte numberOfCores = 4;
+		short typeOfSystem = 64;
+		int	systemMemory = 512;  
+		long dataMemory = 1000; 
+		float randomAccessMemory = 8.0f; 
+		double frequency = 2.80; 
+		char indexCore = 'x';
+		boolean mouse = true;
+
+		System.out.print("Количество ядер CPU - " + numberOfCores + indexCore + " ядерный\nЧастота CPU - " + frequency + " GHz\nRAM - " + randomAccessMemory
+				+ " GB\nПамять SSD - " + systemMemory + " GB\nПамять HDD - " + dataMemory + " GB\nРазрядность ОС - " + indexCore + typeOfSystem + "\nНаличие мыши - "
+				+ mouse);
+	}	
+}
