8a4d4afc92d19720d4f5b323a16eb3291123c5b2
Andrey Falkovskiy <avf-91@yandex.ru>
2021-12-05 22:26:20 +0300

    implement HW_7

--- Lesson_2/Wolf.java
+++ Lesson_2/Wolf.java
@@ -1,9 +1,54 @@
 public class Wolf {
-    String gender = "male";
-    String name = "Akella";
-    float weight = 65.8f;
-    int age = 19;
-    String color = "gray";
+    private String gender = "male";
+    private String name = "Akella";
+    private float weight = 65.8f;
+    private int age = 19;
+    private String color = "gray";
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if(age > 8) {
+            System.out.println("Некорректный возраст.");
+        } else {
+            this.age = age;
+        }
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
 
     public void go() {
         System.out.println("Волк " + name + " идет.");
--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -1,13 +1,13 @@
 public class WolfTest {
     public static void main(String[] args) {
         Wolf wolf = new Wolf();
-        System.out.println("Имя волка: " + wolf.name);
-        System.out.println("Пол волка: " + wolf.gender);
-        System.out.println("Цвет волка: " + wolf.color);
-        wolf.weight = 67.4f;
-        System.out.println("Вес волка: " + wolf.weight);
-        wolf.age = 14;
-        System.out.println("Возраст волка: " + wolf.age);
+        System.out.println("Кличка волка: " + wolf.getName());
+        System.out.println("Пол волка: " + wolf.getGender());
+        System.out.println("Цвет волка: " + wolf.getColor());
+        wolf.setWeight(67.4f);
+        System.out.println("Вес волка: " + wolf.getWeight());
+        wolf.setAge(14);
+        System.out.println("Возраст волка: " + wolf.getAge());
 
         wolf.run();
         wolf.sit();
