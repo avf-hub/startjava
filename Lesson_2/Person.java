1cca5892e87f48744c00d5543ffd32226fa35915
Andrey Falkovskiy <avf-91@yandex.ru>
2021-12-05 12:26:34 +0300

    implement HW_5

---
+++ Lesson_2/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+    String gender = "male";
+    String name = "Oks";
+    float height = 1.84f;
+    float weight = 78.2f;
+    int age = 30;
+
+    public void walk() {
+        System.out.println("Я гуляю на улице.");
+    }
+
+    public void sitDown() {
+        System.out.println("Я присел отдохнуть.");
+    }
+
+    public void run() {
+        System.out.println("Я побежал.");
+    }
+
+    public void speak() {
+        System.out.println("Я разговариваю.");
+    }
+
+    public void learnJava() {
+        System.out.println("Я учу Java.");
+    }
+}
