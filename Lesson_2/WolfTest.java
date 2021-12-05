b2886c45fee1f513e04fb4f6c48973dbe40150b5
Andrey Falkovskiy <avf-91@yandex.ru>
2021-12-05 19:27:40 +0300

    implement HW_6 (fix_1)

--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -1,13 +1,13 @@
 public class WolfTest {
     public static void main(String[] args) {
         Wolf wolf = new Wolf();
-        System.out.println(wolf.name);
-        System.out.println(wolf.gender);
-        System.out.println(wolf.color);
+        System.out.println("Имя волка: " + wolf.name);
+        System.out.println("Пол волка: " + wolf.gender);
+        System.out.println("Цвет волка: " + wolf.color);
         wolf.weight = 67.4f;
-        System.out.println(wolf.weight);
+        System.out.println("Вес волка: " + wolf.weight);
         wolf.age = 14;
-        System.out.println(wolf.age);
+        System.out.println("Возраст волка: " + wolf.age);
 
         wolf.run();
         wolf.sit();
