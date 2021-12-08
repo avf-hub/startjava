fd3c5cdee67e7ceb1a87e33657a7309cb144275e
Andrey Falkovskiy <avf-91@yandex.ru>
2021-12-08 22:20:36 +0300

    implement HW_9

---
+++ Lesson_2/Jaeger.java
@@ -0,0 +1,53 @@
+public class Jaeger {
+    private String modelName;
+    private String mark;
+    private String origin;
+    private float height;
+    private float weight;
+    private int power;
+    private int armor;
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public void setArmor(int armor) {
+        this.armor = armor;
+    }
+
+    public void move(){
+
+    }
+
+    public void scanKaiju() {
+
+    }
+
+    public void attackKaiju() {
+
+    }
+}
---
+++ Lesson_2/JaegerTest.java
@@ -0,0 +1,29 @@
+public class JaegerTest {
+    public static void main(String[] args) {
+        Jaeger jaegerOne = new Jaeger();
+        jaegerOne.setModelName("Gipsy");
+        jaegerOne.setMark("Mark-3");
+        jaegerOne.setOrigin("USA");
+        jaegerOne.setHeight(2.6f);
+        jaegerOne.setWeight(1.98f);
+        jaegerOne.setPower(8);
+        jaegerOne.setArmor(6);
+        jaegerOne.move();
+        jaegerOne.scanKaiju();
+        jaegerOne.attackKaiju();
+        System.out.println("Hello, my name is " + jaegerOne.getModelName());
+
+        Jaeger jaegerTwo = new Jaeger();
+        jaegerTwo.setModelName("Coyote Tango");
+        jaegerTwo.setMark("Mark-5");
+        jaegerTwo.setOrigin("China");
+        jaegerTwo.setHeight(2.8f);
+        jaegerTwo.setWeight(2.312f);
+        jaegerTwo.setPower(7);
+        jaegerTwo.setArmor(4);
+        jaegerTwo.move();
+        jaegerTwo.scanKaiju();
+        jaegerTwo.attackKaiju();
+        System.out.println("Hello, my name is " + jaegerTwo.getModelName());
+    }
+}
