9da33de85a5387786acdb84eae932f743f6e82b2
Andrey Falkovskiy <avf-91@yandex.ru>
2021-12-08 21:31:44 +0300

    implement HW_8 (fix_1)

--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -2,26 +2,34 @@ import java.util.Scanner;
 
 public class CalculatorTest {
     public static void main(String[] args) {
+        boolean active = true;
         Calculator calculator = new Calculator();
         Scanner scanner = new Scanner(System.in);
 
-        while (true) {
-            System.out.print("Введите первое число: ");
-            int numberOne = scanner.nextInt();
-            calculator.setNumber1(numberOne);
-            System.out.print("Введите знак математической операции: ");
-            char sign = scanner.next().charAt(0);
-            calculator.setSign(sign);
-            System.out.print("Введите второе число: ");
-            int numberTwo = scanner.nextInt();
-            calculator.setNumber2(numberTwo);
-            System.out.println("Результат вычисления: " + calculator.calculate());
+    do {
+        System.out.print("Введите первое число: ");
+        int numberOne = scanner.nextInt();
+        calculator.setNumber1(numberOne);
+        System.out.print("Введите знак математической операции: ");
+        char sign = scanner.next().charAt(0);
+        calculator.setSign(sign);
+        System.out.print("Введите второе число: ");
+        int numberTwo = scanner.nextInt();
+        calculator.setNumber2(numberTwo);
+        System.out.println("Результат вычисления: " + calculator.calculate());
+        while(true) {
             System.out.print("Хотите продолжить вычисления? [yes/no]: ");
             String question = scanner.next();
             if(question.equals("no")) {
+                active = false;
                 break;
+            } else if(question.equals("yes")) {
+                break;
+            } else {
+                System.out.println("Вы ввели недопустимый ответ!");
             }
         }
-        System.out.println("Программа завершена!");
-    }
+    } while (active);
+    System.out.println("Программа завершена!");
+}
 }
