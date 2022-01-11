6d71280a29a932bc4263186467e122ecfbe81f45
Andrey Falkovskiy <avf-91@yandex.ru>
2022-01-11 23:22:28 +0300

    implement HW_14 (part 1)

---
+++ src/com/startjava/lesson_5/create_db.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE jaegers;
+
+CREATE TABLE jaegers (
+    id SERIAL PRIMARY KEY,
+    modelName TEXT,
+    mark CHAR(6),
+    height FLOAT,
+    weight FLOAT,
+    status TEXT,
+    origin TEXT,
+    launch DATE,
+    kaijuKill INTEGER);
---
+++ src/com/startjava/lesson_5/init_db.sql
@@ -0,0 +1,3 @@
+INSERT INTO jaegers (modelName, mark, height, weight, status, origin, launch, kaijuKill)
+
+VALUES ('Gipsy Danger', 'Mark-3', 260.0, 1.980, 'Destroyed', 'United States of America', '2017-10-07', 9);
